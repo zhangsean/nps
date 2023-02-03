f7f26ce7eb144f17789952e4d629d50e8e2a1477
ZhangSean <zxf2342@qq.com>
2023-02-03 11:55:40 +0800

    修正暂停删除按钮失效

--- web/static/js/language.js
+++ web/static/js/language.js
@@ -145,7 +145,9 @@ function goback() {
 function submitform(action, url, postdata) {
     postsubmit = false;
 	$.each(postdata, function(i, v){
-		v['value'] = v['value'].trim();
+		if (v['value']) {
+			v['value'] = v['value'].trim();
+		}
 	});
     switch (action) {
         case 'start':
