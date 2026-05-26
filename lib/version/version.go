c526f510ec219d211dc14871dbf8b1b53e49c366
ZhangSean <zxf2342@qq.com>
2026-05-27 00:26:05 +0800

    chore(version): bump version to 0.27.11
    更新版本号从0.27.9到0.27.11

--- lib/version/version.go
+++ lib/version/version.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-const VERSION = "0.27.9"
+const VERSION = "0.27.11"
 
 // Compulsory minimum version, Minimum downward compatibility to this version
 func GetVersion() string {
