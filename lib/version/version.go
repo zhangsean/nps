c07bc752e72d5b1d04074e9bd7f27773b2400a90
ZhangSean <zxf2342@qq.com>
2026-09-04 11:21:44 +0800

    发布 v0.27.28

--- lib/version/version.go
+++ lib/version/version.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-const VERSION = "0.27.27"
+const VERSION = "0.27.28"
 
 // Compulsory minimum version, Minimum downward compatibility to this version
 func GetVersion() string {
